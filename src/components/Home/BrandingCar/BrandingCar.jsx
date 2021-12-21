82baa4eeb954283e9606680ea3aa6c9c1770e29b
haseebDotJs <haseeb.khalid03452800681@gmail.com>
2021-12-21 13:13:55 +0500

    small bug fix

--- src/components/Home/BrandingCar/BrandingCar.jsx
+++ src/components/Home/BrandingCar/BrandingCar.jsx
@@ -5,7 +5,6 @@ import { StaticImage } from "gatsby-plugin-image"
 const BrandingCar = () => {
     return (
         <section>
-        
             <StaticImage layout="fullWidth" src={"../../../images/case-studies-ab-tyres-branding-car.png"}  alt="case studies braning car" placeholder="blurred" />
         </section>
     )
